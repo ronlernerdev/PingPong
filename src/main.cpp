fc9b40d74f59a209f2cc158ba8f9aa58ca70a65c
Ron Lerner <97751296+ronlernerdev@users.noreply.github.com>
2026-09-07 01:55:40 -0300

    i dont get why cpu ai dont work will revert to tutorial on main for now

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,66 @@ public:
     void Update(){
         x += speed_x;
         y += speed_y;
+
+        if (y + radius >= GetScreenHeight() || y - radius <= 0){
+            speed_y *= -1;
+        }
+        if (x + radius >= GetScreenWidth() || x - radius <= 0){
+            speed_x *= -1;
+        }
+    }
+
+};
+class Paddle{
+public:
+    float x,y;
+    float width, height;
+    int speed;
+    int orgin;
+
+    void Draw(){
+        DrawRectangle(x, y, width, height, WHITE);
+    }
+    void Update(){
+        if(IsKeyDown(KEY_UP)){
+            y = y - speed;
+        }
+        if(IsKeyDown(KEY_DOWN)){
+            y = y + speed;
+        }
+
+        if(y <= 0){
+            y = 0;
+        }
+        if(y + height >= GetScreenHeight()){
+            y = GetScreenHeight() - height;
+        }
     }
 
 };
 
+class CpuPaddle: public Paddle{
+public:
+    void Update(int ball_y){
+        if(orgin > ball_y){
+            y = y - speed;
+        }
+        if(orgin <= ball_y){
+            y = y + speed;
+        }
+
+        if(y <= 0){
+            y = 0;
+        }
+        if(y + height >= GetScreenHeight()){
+            y = GetScreenHeight() - height;
+        }
+    }
+};
+
 Ball ball;
+Paddle player;
+CpuPaddle cpu;
 
 
 int main(){
@@ -32,21 +87,37 @@ int main(){
     ball.speed_x = 7;
     ball.speed_y = 7;
 
+    player.width = 25;
+    player.height = 120;
+    player.x = screen_width - player.width - 10;
+    player.y = screen_height/2 - player.height/2;
+    player.speed = 6;
+    player.orgin = player.y + player.height/2;
+
+    cpu.width = 25;
+    cpu.height = 120;
+    cpu.x = 10;
+    cpu.y = screen_height/2 - cpu.height/2;
+    cpu.speed = 6;
+    cpu.orgin = cpu.y + cpu.height/2;
+
     while(WindowShouldClose() == false){
 
         BeginDrawing();
 
         //Updating
         ball.Update();
+        player.Update();
+        cpu.Update(ball.y);
 
         //Drawing
         ClearBackground(BLACK); //Clears Ball Trace
 
         ball.Draw();
+        player.Draw();
+        cpu.Draw();
 
         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
-        DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);
-        DrawRectangle(screen_width - 35, screen_height/2 - 60, 25, 120, WHITE);
 
         EndDrawing();
 
